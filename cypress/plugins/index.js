c2e3d8d2e9e830263fb9af35a48e7736cd3410d8
benyamin <benyamin.rafaeli@gmail.com>
2020-08-08 21:27:37 +0300

    wip Cypress.PluginConfig

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -15,7 +15,17 @@
 /**
  * @type {Cypress.PluginConfig}
  */
+// module.exports = (on, config) => {
+//   // `on` is used to hook into various events Cypress emits
+//   // `config` is the resolved Cypress config
+// }
+
 module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
+  on('before:browser:launch', (browser = {}, args) => {
+    if (browser.name === 'chrome') {
+      args.push('--disable-dev-shm-usage')
+      return args
+    }
+    return args
+  })
 }
